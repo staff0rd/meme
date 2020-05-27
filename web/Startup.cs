a6dfb227a2e6c2d817de2f66e132b7f0879abb22
Stafford Williams <stafford.williams@gmail.com>
2020-05-27 21:46:29 +1000

    remove https

--- web/Startup.cs
+++ web/Startup.cs
@@ -42,11 +42,8 @@ namespace web
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
             app.UseStaticFiles(new StaticFileOptions
